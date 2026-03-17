0dcf140ee460e313d716e65fbeddbf24cbab8343
Sarath Vadlamannati <37393312+sarathvad1811@users.noreply.github.com>
2026-03-17 11:35:17 +0530

    no jekyll

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true // Disable default image optimization
+  },
   onDemandEntries: {
     maxInactiveAge: 60 * 1000,
     pagesBufferLength: 5
